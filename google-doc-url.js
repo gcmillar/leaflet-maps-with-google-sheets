07d9ea603214cc3a30a9cddebc17f399575d350c
gcmillar <gcmillar@ncsu.edu>
2019-01-28 11:24:27 -0500

    change sheet url

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1qUKHIVvYhhrKIsffbOqXs40sWBqRspCLoG-ulZ-Tj2E/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1qUKHIVvYhhrKIsffbOqXs40sWBqRspCLoG-ulZ-Tj2E/edit#gid=0';
